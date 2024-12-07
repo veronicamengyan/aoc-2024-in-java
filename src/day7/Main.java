729d6b18831fb06e586be4d910c38b02d3b11684
Veronica Lusby <vlusby@indeed.com>
2024-12-07 17:53:23 -0600

    day7

--- src/day7/Main.java
+++ src/day7/Main.java
@@ -3,84 +3,63 @@ package day7;
 import util.Utility;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(final String[] args) throws IOException {
         final List<String> inputs = Utility.readFile("day7", "input1");
-        part1(inputs);
-        final Map<CardType, List<Hand>> cardTypeHandMap = new TreeMap<>(Comparator.comparingInt(CardType::ordinal).reversed());
-        for (final String hand : inputs) {
-            final String[] cardBid = hand.split("\\s+");
-            final String card = cardBid[0].trim();
-            final int bid = Integer.parseInt(cardBid[1].trim());
-            final CardType cardType = CardType.fromStringPart2(card);
-            if (cardTypeHandMap.containsKey(cardType)) {
-                cardTypeHandMap.get(cardType).add(new Hand(card, bid));
-            } else {
-                cardTypeHandMap.put(cardType, new ArrayList<>(List.of(new Hand(card, bid))));
+        final char[] operations1 = {'+', '*' };
+        final char[] operations2 = {'+', '*', 'c' };
+
+        //part1
+        part(inputs, operations1);
+
+        //part2
+        part(inputs, operations2);
+    }
+
+    public static void part(final List<String> inputs, final char[] operations) {
+        long sum = 0;
+        for (final String input : inputs) {
+            final String[] parts = input.split(":");
+            final long targetValue = Long.parseLong(parts[0]);
+            final Long[] inputValues = Arrays.stream(parts[1].trim().split(" "))
+                    .map(String::trim)
+                    .map(Long::parseLong)
+                    .toArray(Long[]::new);
+            if (correct(targetValue, inputValues, operations)) {
+                sum += targetValue;
             }
         }
-
-        int sum = 0;
-        int rank = 1;
-        for (final List<Hand> hands : cardTypeHandMap.values()) {
-            hands.sort((a, b) -> {
-                for (int i = 0; i < a.getCard().length(); i++) {
-                    final Comparator<Character> charComparator = Comparator
-                            .<Character, Integer>comparing(c -> "AKQT98765432J".indexOf((char) c))
-                            .reversed();
-                    final int compare = charComparator.compare(a.getCard().charAt(i), b.getCard().charAt(i));
-                    if (compare != 0) {
-                        return compare;
-                    }
-                }
-                return 0;
-            });
-            for (final Hand hand : hands) {
-                sum += hand.getBid() * rank;
-                rank++;
-            }
-        }
-
         System.out.println(sum);
     }
 
-    public static void part1(final List<String> inputs) {
-        final Map<CardType, List<Hand>> cardTypeHandMap = new TreeMap<>(Comparator.comparingInt(CardType::ordinal).reversed());
-        for (final String hand : inputs) {
-            final String[] cardBid = hand.split("\\s+");
-            final String card = cardBid[0].trim();
-            final int bid = Integer.parseInt(cardBid[1].trim());
-            final CardType cardType = CardType.fromString(card);
-            if (cardTypeHandMap.containsKey(cardType)) {
-                cardTypeHandMap.get(cardType).add(new Hand(card, bid));
+    public static boolean correct(final long targetValue, final Long[] inputValues, char[] operations) {
+        return checkRecursively(targetValue, inputValues, 0, 0, operations);
+    }
+
+    public static boolean checkRecursively(final long targetValue, final Long[] inputValues, final long result, final int index, char[] operations) {
+        if (index == inputValues.length) {
+            return result == targetValue;
+        }
+        for (final char operation : operations) {
+            boolean isValid;
+            if (operation == '+') {
+                isValid = checkRecursively(targetValue, inputValues, result + inputValues[index], index + 1, operations);
+            } else if (operation == '*') {
+                isValid = checkRecursively(targetValue, inputValues, result * inputValues[index], index + 1, operations);
             } else {
-                cardTypeHandMap.put(cardType, new ArrayList<>(List.of(new Hand(card, bid))));
+                isValid = checkRecursively(targetValue, inputValues, concatenate(result, inputValues[index]), index + 1, operations);
+            }
+            if (isValid) {
+                return true;
             }
         }
+        return false;
+    }
 
-        int sum = 0;
-        int rank = 1;
-        for (final List<Hand> hands : cardTypeHandMap.values()) {
-            hands.sort((a, b) -> {
-                for (int i = 0; i < a.getCard().length(); i++) {
-                    Comparator<Character> charComparator = Comparator
-                            .<Character, Integer>comparing(c -> "AKQJT98765432".indexOf((char) c))
-                            .reversed();
-                    final int compare = charComparator.compare(a.getCard().charAt(i), b.getCard().charAt(i));
-                    if (compare != 0) {
-                        return compare;
-                    }
-                }
-                return 0;
-            });
-            for (final Hand hand : hands) {
-                sum += hand.getBid() * rank;
-                rank++;
-            }
-        }
-
-        System.out.println(sum);
+    public static long concatenate(final long result, final long value) {
+        return Long.parseLong(result + "" + value);
     }
 }
