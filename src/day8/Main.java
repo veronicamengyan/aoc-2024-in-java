b0e5f70c7f153bfd040068f48ff366e2b0e9bbec
Veronica Lusby <vlusby@indeed.com>
2024-12-08 12:18:58 -0600

    day8-part2

--- src/day8/Main.java
+++ src/day8/Main.java
@@ -9,10 +9,11 @@ import java.util.*;
 public class Main {
     public static void main(final String[] args) throws IOException {
         final char[][] grid = Utility.readFileIntoGrid("day8", "input1");
-        part1(grid);
+        part(grid, true);
+        part(grid, false);
     }
 
-    public static void part1(final char[][] grid) {
+    public static void part(final char[][] grid, final boolean isPart1) {
         final Map<Character, List<Pair>> antennas = new HashMap<>();
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
@@ -24,8 +25,7 @@ public class Main {
             }
         }
         final Set<Pair> antinodes = new HashSet<>();
-        for (final Character key : antennas.keySet()) {
-            final List<Pair> coordinates = antennas.get(key);
+        for (final List<Pair> coordinates : antennas.values()) {
             // loop through any two coordinates
             for (int k = 0; k < coordinates.size(); k++) {
                 for (int l = k + 1; l < coordinates.size(); l++) {
@@ -36,24 +36,42 @@ public class Main {
                     final int xDiff = p1.getFirst() - p2.getFirst();
                     final int yDiff = p1.getSecond() - p2.getSecond();
 
-                    // antinode1
-                    final int xAntinode = p1.getFirst() + xDiff;
-                    final int yAntinode = p1.getSecond() + yDiff;
+                    if (isPart1) {
+                        // antinode1
+                        final int xAntinode = p1.getFirst() + xDiff;
+                        final int yAntinode = p1.getSecond() + yDiff;
 
-                    if ((xAntinode >= 0) && (xAntinode < grid.length) && (yAntinode >= 0) && (yAntinode < grid[0].length)) {
-                        antinodes.add(new Pair(xAntinode, yAntinode));
-                    }
+                        if (withinBounds(xAntinode, yAntinode, grid)) {
+                            antinodes.add(new Pair(xAntinode, yAntinode));
+                        }
 
-                    // antinode2
-                    final int xAntinode2 = p2.getFirst() - xDiff;
-                    final int yAntinode2 = p2.getSecond() - yDiff;
+                        // antinode2
+                        final int xAntinode2 = p2.getFirst() - xDiff;
+                        final int yAntinode2 = p2.getSecond() - yDiff;
 
-                    if ((xAntinode2 >= 0) && (xAntinode2 < grid.length) && (yAntinode2 >= 0) && (yAntinode2 < grid[0].length)) {
-                        antinodes.add(new Pair(xAntinode2, yAntinode2));
+                        if (withinBounds(xAntinode2, yAntinode2, grid)) {
+                            antinodes.add(new Pair(xAntinode2, yAntinode2));
+                        }
+                    } else {
+                        for (int i = 0; i < grid.length; i++) {
+                            for (int j = 0; j < grid[0].length; j++) {
+                                if (sameLine(i, j, xDiff, yDiff, p1.getFirst(), p1.getSecond())) {
+                                    antinodes.add(new Pair(i, j));
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
         System.out.println(antinodes.size());
+    }
+
+    private static boolean withinBounds(final int x, final int y, final char[][] grid) {
+        return (x >= 0) && (x < grid.length) && (y >= 0) && (y < grid[0].length);
+    }
+
+    private static boolean sameLine(final int x, final int y, final int xDiff, final int yDiff, final int xAntenna, final int yAntenna) {
+        return (xDiff * (y - yAntenna)) == (yDiff * (x - xAntenna));
     }
 }
