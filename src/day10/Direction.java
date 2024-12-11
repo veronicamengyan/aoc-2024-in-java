81c1702de7f8d380552d4bdeebbe197119fc51c7
Veronica Lusby <vlusby@indeed.com>
2024-12-10 22:07:05 -0600

    day10

--- src/day10/Direction.java
+++
@@ -1,8 +0,0 @@
-package day10;
-
-public enum Direction {
-    DOWN,
-    UP,
-    LEFT,
-    RIGHT
-}
--- src/day10/Main.java
+++ src/day10/Main.java
@@ -1,144 +1,75 @@
 package day10;
 
+import util.Pair;
 import util.Utility;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 
 public class Main {
     public static void main(final String[] args) throws IOException {
-        final List<String> inputs = Utility.readFile("day10", "input1");
-        final char[][] grid = new char[inputs.size()][inputs.get(0).length()];
-        int i = 0;
-        int startRow = -1;
-        int startCol = -1;
-        for (String input : inputs) {
-            int j = 0;
-            for (Character symbol : input.toCharArray()) {
-                grid[i][j] = symbol;
-                if (symbol == 'S') {
-                    startRow = i;
-                    startCol = j;
+        final char[][] inputs = Utility.readFileIntoGrid("day10", "input1");
+        final List<Pair> trailHeads = new ArrayList<>();
+        for (int i = 0; i < inputs.length; i++) {
+            for (int j = 0; j < inputs[0].length; j++) {
+                if (inputs[i][j] == '0') {
+                    trailHeads.add(new Pair(i, j));
                 }
-                j++;
             }
-            i++;
         }
 
-        //part1
+        //part 1
         int sum = 0;
-
-        sum += followPath(Direction.DOWN, grid, startRow + 1, startCol);
-        System.out.println((sum + 1) / 2);
+        for (int i = 0; i < trailHeads.size(); i++) {
+            final Set<Pair> reachableDestinations = new HashSet<>();
+            findPaths(trailHeads.get(i), inputs, '0', reachableDestinations);
+            sum += reachableDestinations.size();
+        }
+        System.out.println(sum);
 
         //part2
-        grid[startRow][startCol] = 'T'; // it is kinda cheating here because I know S would be '7' based on the input
-
-        int area = 0;
-        for (int row = 0; row < grid.length; row++) {
-            for (int col = 0; col < grid[0].length; col++) {
-                if ("PT".indexOf(Character.toString(grid[row][col])) >= 0) {
-                    continue;
-                }
-                if (isInsideLoop(grid, row, col)) {
-                    area++;
-                }
-            }
+        int sum2 = 0;
+        for (int i = 0; i < trailHeads.size(); i++) {
+            sum2 += findRating(trailHeads.get(i), inputs, '0', 0);
         }
-        System.out.println(area);
+        System.out.println(sum2);
     }
 
-    private static boolean isInsideLoop(final char[][] grid, int row, int col) {
-        int pathCounter = 0;
-        int rayRow = row;
-        int rayCol = col;
-        while (rayRow < grid.length && rayCol < grid[0].length) {
-            char cursor = grid[rayRow][rayCol];
-            if (cursor == 'P') {
-                pathCounter++;
-            }
-            rayRow++;
-            rayCol++;
+    private static void findPaths(final Pair position, final char[][] inputs, final char startingLetter, final Set<Pair> reachableDestinations) {
+        if ((position.getFirst() < 0) || (position.getFirst() >= inputs.length) || (position.getSecond() < 0) || (position.getSecond() >= inputs[0].length)) {
+            return;
         }
-        return (pathCounter % 2) != 0;
+        if ((startingLetter == '9') && (inputs[position.getFirst()][position.getSecond()] == '9')) {
+            reachableDestinations.add(new Pair(position.getFirst(), position.getSecond()));
+            return;
+        }
+        if (inputs[position.getFirst()][position.getSecond()] != startingLetter) {
+            return;
+        }
+        final char nextLetter = (char) (startingLetter + 1);
+        findPaths(new Pair(position.getFirst() + 1, position.getSecond()), inputs, nextLetter, reachableDestinations);
+        findPaths(new Pair(position.getFirst() - 1, position.getSecond()), inputs, nextLetter, reachableDestinations);
+        findPaths(new Pair(position.getFirst(), position.getSecond() + 1), inputs, nextLetter, reachableDestinations);
+        findPaths(new Pair(position.getFirst(), position.getSecond() - 1), inputs, nextLetter, reachableDestinations);
     }
 
-    private static int followPath(final Direction direction, final char[][] grid, int row, int col) {
-        int curRow = row;
-        int curCol = col;
-        int counter = 0;
-        Direction dir = direction;
-        while (grid[curRow][curCol] != 'S') {
-            char temp = grid[curRow][curCol];
-            grid[curRow][curCol] = 'P';
-            if (dir == Direction.DOWN) {
-                switch (temp) {
-                    case '|' -> {
-                        dir = Direction.DOWN;
-                        curRow += 1;
-                    }
-                    case 'L' -> {
-                        dir = Direction.RIGHT;
-                        grid[curRow][curCol] = 'T';
-                        curCol += 1;
-                    }
-                    case 'J' -> {
-                        dir = Direction.LEFT;
-                        curCol -= 1;
-                    }
-                }
-            } else if (dir == Direction.UP) {
-                switch (temp) {
-                    case '|' -> {
-                        dir = Direction.UP;
-                        curRow -= 1;
-                    }
-                    case '7' -> {
-                        dir = Direction.LEFT;
-                        grid[curRow][curCol] = 'T';
-                        curCol -= 1;
-                    }
-                    case 'F' -> {
-                        dir = Direction.RIGHT;
-                        curCol += 1;
-                    }
-                }
-            } else if (dir == Direction.LEFT) {
-                switch (temp) {
-                    case '-' -> {
-                        dir = Direction.LEFT;
-                        curCol -= 1;
-                    }
-                    case 'L' -> {
-                        dir = Direction.UP;
-                        grid[curRow][curCol] = 'T';
-                        curRow -= 1;
-                    }
-                    case 'F' -> {
-                        dir = Direction.DOWN;
-                        curRow += 1;
-                    }
-                }
-            } else if (dir == Direction.RIGHT) {
-                switch (temp) {
-                    case '-' -> {
-                        dir = Direction.RIGHT;
-                        curCol += 1;
-                    }
-                    case 'J' -> {
-                        dir = Direction.UP;
-                        curRow -= 1;
-                    }
-                    case '7' -> {
-                        dir = Direction.DOWN;
-                        grid[curRow][curCol] = 'T';
-                        curRow += 1;
-                    }
-                }
-            }
-            counter++;
+    private static int findRating(final Pair position, final char[][] inputs, final char startingLetter, final int rating) {
+        if ((position.getFirst() < 0) || (position.getFirst() >= inputs.length) || (position.getSecond() < 0) || (position.getSecond() >= inputs[0].length)) {
+            return rating;
         }
-        return counter;
+        if ((startingLetter == '9') && (inputs[position.getFirst()][position.getSecond()] == '9')) {
+            return rating + 1;
+        }
+        if (inputs[position.getFirst()][position.getSecond()] != startingLetter) {
+            return rating;
+        }
+        final char nextLetter = (char) (startingLetter + 1);
+        return findRating(new Pair(position.getFirst() + 1, position.getSecond()), inputs, nextLetter, rating) +
+                findRating(new Pair(position.getFirst() - 1, position.getSecond()), inputs, nextLetter, rating) +
+                findRating(new Pair(position.getFirst(), position.getSecond() + 1), inputs, nextLetter, rating) +
+                findRating(new Pair(position.getFirst(), position.getSecond() - 1), inputs, nextLetter, rating);
     }
 }
