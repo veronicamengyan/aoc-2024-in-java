747002afb37202cd2935a4b4196e74137017873e
Veronica Lusby <vlusby@indeed.com>
2024-12-13 10:41:31 -0600

    day13

--- src/day13/Main.java
+++ src/day13/Main.java
@@ -1,104 +1,77 @@
 package day13;
 
+import util.Pair;
 import util.Utility;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(final String[] args) throws IOException {
         final List<String> inputs = Utility.readFile("day13", "input1");
-        final List<List<String>> grids = new ArrayList<>();
-        List<String> grid = new ArrayList<>();
-        for (final String line : inputs) {
-            if (line.isEmpty() && grid.size() > 0) {
-                grids.add(grid);
-                grid = new ArrayList<>();
+        final List<List<String>> machines = new ArrayList<>();
+        List<String> machine = new ArrayList<>();
+        machines.add(machine);
+        for (final String input : inputs) {
+            if (input.trim().isEmpty()) {
+                machine = new ArrayList<>();
+                machines.add(machine);
             } else {
-                grid.add(line);
-            }
-        }
-        if (grid.size() > 0) {
-            grids.add(grid);
-        }
-
-        int sum = 0;
-        for (final List<String> block : grids) {
-
-            // check horizontal
-            int horizontalMirror = -1;
-            for (int m = 1; m < block.size(); m++) {
-                boolean isReflection = true;
-                final int size = Math.min(m, block.size() - m);
-                int diff = 0;
-                for (int i = 0; i < size; i++) {
-                    final String up = block.get(m - i - 1);
-                    final String down = block.get(m + i);
-
-                    //part1
-//                    if (!up.equals(down)) {
-//                        isReflection = false;
-//                        break;
-//                    }
-                    diff += difference(up, down);
-                }
-                if (diff == 1) {
-                    horizontalMirror = m;
-                    break;
-                }
-            }
-            if (horizontalMirror > 0) {
-                sum += 100 * horizontalMirror;
-            }
-
-            //check vertical
-            if (horizontalMirror == -1) {
-                int verticalMirror = -1;
-                for (int m = 1; m < block.get(0).length(); m++) {
-                    boolean isReflection = true;
-                    final int size = Math.min(m, block.get(0).length() - m);
-                    int diff = 0;
-                    for (int i = 0; i < size; i++) {
-                        int finalM = m;
-                        int finalI = i;
-                        final String left = block.stream().map(str -> str.charAt(finalM - finalI - 1)).collect(Collectors.toList()).toString();
-                        final String right = block.stream().map(str -> str.charAt(finalM + finalI)).collect(Collectors.toList()).toString();
-
-                        //part1
-//                        if (!left.equals(right)) {
-//                            isReflection = false;
-//                            break;
-//                        }
-                        diff += difference(left, right);
-                    }
-                    if (diff == 1) {
-                        verticalMirror = m;
-                    }
-                }
-                if (verticalMirror > 0) {
-                    sum += verticalMirror;
-                }
+                machine.add(input);
             }
         }
 
+        //part1
+        System.out.println(machines.stream().mapToLong(Main::findTokens).sum());
 
-        System.out.println(sum);
+        //part2
+        System.out.println(machines.stream().mapToLong(Main::findTokens2).sum());
     }
 
-    private static int difference(String str1, String str2) {
-        int i = 0;
-        int j = 0;
-        int diff = 0;
-        while (i < str1.length() && j < str2.length()) {
-            if (str1.charAt(i) != str2.charAt(j)) {
-                diff++;
-            }
-            i++;
-            j++;
-        }
-        return diff;
+    public static long findTokens(final List<String> machine) {
+        final Pair A = findPair(machine.get(0), "Button A: X\\+(\\d+), Y\\+(\\d+)");
+        final Pair B = findPair(machine.get(1), "Button B: X\\+(\\d+), Y\\+(\\d+)");
+        final Pair prize = findPair(machine.get(2), "Prize: X\\=(\\d+), Y\\=(\\d+)");
+        final int determinant1 = (A.getFirst() * B.getSecond()) - (A.getSecond() * B.getFirst());
+        final int determinant2 = (prize.getFirst() * B.getSecond()) - (B.getFirst() * prize.getSecond());
+        final int determinant3 = (A.getFirst() * prize.getSecond()) - (A.getSecond() * prize.getFirst());
 
+        if (determinant2 % determinant1 != 0 || determinant3 % determinant1 != 0) {
+            return 0;
+        }
+
+        final int x = determinant2 / determinant1;
+        final int y = determinant3 / determinant1;
+
+        return (3L * x) + y;
+    }
+
+    public static long findTokens2(final List<String> machine) {
+        final Pair A = findPair(machine.get(0), "Button A: X\\+(\\d+), Y\\+(\\d+)");
+        final Pair B = findPair(machine.get(1), "Button B: X\\+(\\d+), Y\\+(\\d+)");
+        final Pair prize = findPair(machine.get(2), "Prize: X\\=(\\d+), Y\\=(\\d+)");
+        final long bigNumber = 10000000000000L;
+        final int determinant1 = (A.getFirst() * B.getSecond()) - (A.getSecond() * B.getFirst());
+        final long determinant2 = ((prize.getFirst() + bigNumber) * B.getSecond()) - (B.getFirst() * (prize.getSecond() + bigNumber));
+        final long determinant3 = (A.getFirst() * (prize.getSecond() + bigNumber)) - (A.getSecond() * (prize.getFirst() + bigNumber));
+        if (((determinant2 % determinant1) != 0) || ((determinant3 % determinant1) != 0)) {
+            return 0;
+        }
+        final long x = determinant2 / determinant1;
+        final long y = determinant3 / determinant1;
+        return (3 * x) + y;
+    }
+
+    public static Pair findPair(final String line, final String regex) {
+        final Pattern pattern = Pattern.compile(regex);
+        final Matcher matcher = pattern.matcher(line);
+        if (matcher.find()) {
+            return new Pair(Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2)));
+        } else {
+            throw new IllegalArgumentException("No match found");
+        }
     }
 }
